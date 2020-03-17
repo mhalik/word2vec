a01f3782dca235ae46b03872a918fc014e81dc05
Max Halik <max.halik@gordon.edu>
2020-03-17 15:43:15 -0400

    Update word2vec_v1.py

--- word2vec_v1.py
+++ word2vec_v1.py
@@ -82,3 +82,76 @@ for i in range( 0, num_reviews ):
     # clean reviews
     clean_train_reviews.append( review_to_words( train["review"][i] ) )
 
+#%%
+    
+print("Creating the bag of words...\n")
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Initialize the "CountVectorizer" object, which is scikit-learn's
+# bag of words tool.  
+vectorizer = CountVectorizer(analyzer = "word",   \
+                             tokenizer = None,    \
+                             preprocessor = None, \
+                             stop_words = None,   \
+                             max_features = 2000) 
+
+# fit_transform() does two functions: First, it fits the model
+# and learns the vocabulary; second, it transforms our training data
+# into feature vectors. The input to fit_transform should be a list of 
+# strings.
+train_data_features = vectorizer.fit_transform(clean_train_reviews)
+
+# Numpy arrays are easy to work with, so convert the result to an 
+# array
+train_data_features = train_data_features.toarray()
+
+# Take a look at the words in the vocabulary
+vocab = vectorizer.get_feature_names()
+print(vocab)
+
+#%%
+
+print("Training the random forest...")
+from sklearn.ensemble import RandomForestClassifier
+
+# Initialize a Random Forest classifier with 100 trees
+forest = RandomForestClassifier(n_estimators = 40) 
+
+# Fit the forest to the training set, using the bag of words as 
+# features and the sentiment labels as the response variable
+#
+# This may take a few minutes to run
+forest = forest.fit( train_data_features, train["sentiment"] )
+
+#%%
+
+# Read the test data
+test = pd.read_csv(r"word2vec\testData.tsv", header=0, \
+                    delimiter="\t", quoting=3)
+# Verify that there are 25,000 rows and 2 columns
+print(test.shape)
+
+# Create an empty list and append the clean reviews one by one
+num_reviews = len(test["review"])
+clean_test_reviews = [] 
+
+print("Cleaning and parsing the test set movie reviews...\n")
+for i in range(0,num_reviews):
+    if( (i+1) % 1000 == 0 ):
+        print("Review %d of %d\n" % (i+1, num_reviews))
+    clean_review = review_to_words( test["review"][i] )
+    clean_test_reviews.append( clean_review )
+
+# Get a bag of words for the test set, and convert to a numpy array
+test_data_features = vectorizer.transform(clean_test_reviews)
+test_data_features = test_data_features.toarray()
+
+# Use the random forest to make sentiment label predictions
+result = forest.predict(test_data_features)
+
+# Copy the results to a pandas dataframe with an "id" column and
+# a "sentiment" column
+output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
+
+# Use pandas to write the comma-separated output file
+output.to_csv( "Bag_of_Words_model.csv", index=False, quoting=3 )
